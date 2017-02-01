3778c74da2bec9170c3300a2cf04bb0c4b680be1
sirjuan <t6peju01@students.oamk.fi>
2017-02-01 14:12:01 +0200

    test API endpoints

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
-var CONTACTS_COLLECTION = "contacts";
+var POSTS_DATA = "posts";
 
 var app = express();
 app.use(bodyParser.json());
@@ -33,7 +33,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   });
 });
 
-// CONTACTS API ROUTES BELOW
+// POSTS API ROUTES BELOW
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
@@ -41,61 +41,61 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-/*  "/api/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
+/*  "/api/posts"
+ *    GET: finds all posts
+ *    POST: creates a new post
  */
 
-app.get("/api/contacts", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+app.get("/api/posts", function(req, res) {
+  db.collection(POSTS_DATA).find({}).toArray(function(err, docs) {
     if (err) {
-      handleError(res, err.message, "Failed to get contacts.");
+      handleError(res, err.message, "Failed to get posts.");
     } else {
       res.status(200).json(docs);
     }
   });
 });
 
-app.post("/api/contacts", function(req, res) {
-  var newContact = req.body;
-  newContact.createDate = new Date();
+app.post("/api/posts", function(req, res) {
+  var newPost = req.body;
+  newPost.createDate = new Date();
 
   if (!req.body.name) {
     handleError(res, "Invalid user input", "Must provide a name.", 400);
   }
 
-  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+  db.collection(POSTS_DATA).insertOne(newPost, function(err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to create new contact.");
+      handleError(res, err.message, "Failed to create new post.");
     } else {
       res.status(201).json(doc.ops[0]);
     }
   });
 });
 
-/*  "/api/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
+/*  "/api/posts/:id"
+ *    GET: find post by id
+ *    PUT: update post by id
+ *    DELETE: deletes post by id
  */
 
-app.get("/api/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+app.get("/api/posts/:id", function(req, res) {
+  db.collection(POSTS_DATA).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to get contact");
+      handleError(res, err.message, "Failed to get post");
     } else {
       res.status(200).json(doc);
     }
   });
 });
 
-app.put("/api/contacts/:id", function(req, res) {
+app.put("/api/posts/:id", function(req, res) {
   var updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+  db.collection(POSTS_DATA).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to update contact");
+      handleError(res, err.message, "Failed to update post");
     } else {
       updateDoc._id = req.params.id;
       res.status(200).json(updateDoc);
@@ -103,10 +103,10 @@ app.put("/api/contacts/:id", function(req, res) {
   });
 });
 
-app.delete("/api/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+app.delete("/api/posts/:id", function(req, res) {
+  db.collection(POSTS_DATA).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
     if (err) {
-      handleError(res, err.message, "Failed to delete contact");
+      handleError(res, err.message, "Failed to delete post");
     } else {
       res.status(200).json(req.params.id);
     }
--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,23 +1,55 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
+import { Headers, Http, Response  } from '@angular/http';
 import { Storage } from '@ionic/storage';
 import 'rxjs/add/operator/map';
 import { Post } from './post'
 import { POSTS } from './mock-posts';
 import 'rxjs/add/operator/toPromise';
 
-
 @Injectable()
 export class Data {
+    private contactsUrl = '/api/contacts';
 
-   getPosts(): Promise<Post[]> {
-    return Promise.resolve(POSTS);
-  }
+    constructor (private http: Http) {}
 
-  addPosts() {
-    
-  }
+    // get("/api/contacts")
+    getContacts(): Promise<Contact[]> {
+      return this.http.get(this.contactsUrl)
+                 .toPromise()
+                 .then(response => response.json() as Contact[])
+                 .catch(this.handleError);
+    }
 
+    // post("/api/contacts")
+    createContact(newContact: Contact): Promise<Contact> {
+      return this.http.post(this.contactsUrl, newContact)
+                 .toPromise()
+                 .then(response => response.json() as Contact)
+                 .catch(this.handleError);
+    }
+
+    // get("/api/contacts/:id") endpoint not used by Angular app
+
+    // delete("/api/contacts/:id")
+    deleteContact(delContactId: String): Promise<String> {
+      return this.http.delete(this.contactsUrl + '/' + delContactId)
+                 .toPromise()
+                 .then(response => response.json() as String)
+                 .catch(this.handleError);
+    }
+
+    // put("/api/contacts/:id")
+    updateContact(putContact: Contact): Promise<Contact> {
+      var putUrl = this.contactsUrl + '/' + putContact._id;
+      return this.http.put(putUrl, putContact)
+                 .toPromise()
+                 .then(response => response.json() as Contact)
+                 .catch(this.handleError);
+    }
+
+    private handleError (error: any) {
+      let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+      console.error(errMsg); // log to console instead
+    }
 }
-
- 
