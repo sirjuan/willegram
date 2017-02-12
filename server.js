12edc103a72923f3becc1ccb539e3202c98b7768
sirjuan <t6peju01@students.oamk.fi>
2017-02-12 11:08:52 +0200

    getting posts based by followed users

--- server.js
+++ server.js
@@ -87,8 +87,8 @@ res.status(200).json(doc);
 
 // GET: retrieve posts from followed users
 
-app.get("/api/posts/joo/followed", function(req, res) {
-db.collection("posts").find({ userName: { $in: ["wille","sirjuan"]} }).toArray(function(err, docs) {
+app.get("/api/posts/followed/:users", function(req, res) {
+db.collection("posts").find({ userName: { $in: [req.params.users]} }).toArray(function(err, docs) {
 if (err) {
 handleError(res, err.message, "Failed to get posts");
 } else {
